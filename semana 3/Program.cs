4e0c2172c195910ab6d62b82acca41d3894b480f
Claros <Claros@WINDOWS-T3DB4AT>
2021-08-10 14:53:54 -0600

    Agregar archivos de proyecto.

---
+++ semana 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         // CALCULAR LA NOTA PROMEDIO DE N NOTAS
+            int[] notas = new int[10];
+            //capturar las notas
+            for (int i = 0; i < notas.Length; i++)
+            {
+                //solicitar las notas al usuario
+                Console.Write("Escriba la nota {0}: ", i);
+                notas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            //recorrer el arreglo de notas
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.WriteLine("Nota {0}: {1}", i, notas[i]); //imprime los valroes
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
